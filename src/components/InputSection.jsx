ed8dba1eb01303a735753b41d388849261f68502
Surgetin <65120855+Surgetin@users.noreply.github.com>
2022-11-06 20:53:24 +0100

    fully responsive

--- src/components/InputSection.jsx
+++ src/components/InputSection.jsx
@@ -6,9 +6,9 @@ const InputSection = ({isError, shortLink, setShortLink, handleSubmit}) => {
         <div className="input_container" onSubmit={handleSubmit}>
             <form className="form_container">
                 <input type="text" className={isError ? 'input_error' : ''} value={shortLink} onChange={(e) => setShortLink(e.target.value)} placeholder={InputSectionText[0].content}/>
+                <div className={isError ? 'input_error-message active' : 'input_error-message'}>Please enter a valid link</div>
                 <button className="btn submit_btn" type="submit">{InputSectionText[1].content}</button>
             </form>
-            <div className={isError ? 'input_error-message active' : 'input_error-message'}>Please enter a valid link</div>
         </div>
     );
 };
